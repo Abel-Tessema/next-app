5ce019a57a595d08321a0c00d16b8fed542ecd8e
Bela Jash <abelxotessema@gmail.com>
2024-07-09 13:57:40 +0300

    Explore static and dynamic server-side rendering.

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -8,14 +8,14 @@ interface User {
 async function UsersPage() {
   const response = await fetch(
     'https://jsonplaceholder.typicode.com/users',
-    // {cache: 'no-store'}
-    {next: {revalidate: 10}} // Fetch data from the backend every 10 seconds
+    {cache: 'no-store'}
   );
   const users: User[] = await response.json();
 
   return (
     <>
       <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map(user => (
           <li key={user.id}>{user.name}</li>
