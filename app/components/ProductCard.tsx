29e81058257fadaa32fd5f0a795eaf80ba0a033b
Bela Jash <abelxotessema@gmail.com>
2024-07-09 15:49:38 +0300

    Explore daisyUI.

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -2,7 +2,7 @@ import AddToCart from "@/app/components/AddToCart";
 
 function ProductCard() {
   return (
-    <div className='p-5 my-5 bg-sky-500 text-white text-xl hover:bg-sky-400 hover:cursor-pointer'>
+    <div>
       <AddToCart/>
     </div>
   );
