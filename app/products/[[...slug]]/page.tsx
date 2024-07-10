d0dc780eaf497a9430d58b477404f9d7c6a01f0f
Bela Jash <abelxotessema@gmail.com>
2024-07-10 16:01:55 +0300

    Explore varying number of parameters in a route.

---
+++ app/products/[[...slug]]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface Props {
+  params: {slug: string[]}
+}
+
+function ProductPage({params: {slug}}: Props) {
+  return (
+    <div>ProductPage {slug}</div>
+  );
+}
+
+export default ProductPage;
