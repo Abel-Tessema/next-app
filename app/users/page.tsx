a1d8de4d47183549ee7f190884bbce38dd9ce97d
Bela Jash <abelxotessema@gmail.com>
2024-07-10 15:19:57 +0300

    Explore routing.

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,37 +1,10 @@
 import React from 'react';
 
-interface User {
-  id: number,
-  name: string,
-  email: string,
-}
-
 async function UsersPage() {
-  const response = await fetch(
-    'https://jsonplaceholder.typicode.com/users',
-    {cache: 'no-store'}
-  );
-  const users: User[] = await response.json();
-
   return (
     <>
       <h1>Users</h1>
-      <table className='table table-bordered'>
-        <thead>
-        <tr>
-          <th>Name</th>
-          <th>Email</th>
-        </tr>
-        </thead>
-        <tbody>
-          {users.map(user => (
-            <tr key={user.id}>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <UsersPage/>
     </>
   );
 }
