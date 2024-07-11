657b2b26e802446975d2cea95397b9b04e4c9cf6
Bela Jash <abelxotessema@gmail.com>
2024-07-11 16:31:19 +0300

    Explore APIs: updating an object.

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -10,3 +10,21 @@ export function GET(request: NextRequest, {params: {id}}: Props) {
 
   return NextResponse.json({id: 1, name: 'Bela Jash'})
 }
+
+export async function PUT(request: NextRequest, {params: {id}}: Props) {
+  /*
+   * Validate the request body.
+   * If it's invalid, return 400 (bad request).
+   * Else, fetch the user with the given ID.
+     * If it doesn't exist, return 404.
+     * Else, update the user and return it.
+   */
+  const body = await request.json();
+  if (!body.name)
+    return NextResponse.json({error: 'Name is required.'}, {status: 400});
+
+  if (id > 10) // Simulating a user not found error.
+    return NextResponse.json({error: 'User not found.'}, {status: 404});
+
+  return NextResponse.json({id: id, name: body.name}, {status: 200});
+}
