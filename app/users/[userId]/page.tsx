7888537ed3bc2b9705e85439130d1fe6e2d6e675
Bela Jash <abelxotessema@gmail.com>
2024-07-11 13:53:33 +0300

    Explore handling not found errors.

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import {notFound} from "next/navigation";
 
 interface Props {
   params: {userId: number}
 }
 
 function UserDetailPage({params: {userId}} :Props) {
+  if (userId > 10) notFound();
+
   return (
     <div>User {userId}</div>
   );
