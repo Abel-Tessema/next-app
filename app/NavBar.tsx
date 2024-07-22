b36fdf513a8799eda7ddbebf38bf8be262dad7bf
Bela Jash <abelxotessema@gmail.com>
2024-07-22 14:22:03 +0300

    Configure Google Provider.

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,9 +3,10 @@ import Link from "next/link";
 
 function NavBar() {
   return (
-    <div className='flex bg-slate-200 p-5'>
+    <div className='flex bg-slate-200 p-5 space-x-5'>
       <Link href='/' className='mr-5'>Next.js</Link>
       <Link href='users'>Users</Link>
+      <Link href='api/auth/signin'>Log In</Link>
     </div>
   );
 }
