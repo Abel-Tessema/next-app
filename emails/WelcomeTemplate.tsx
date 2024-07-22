0842bbe75a7426a6986b3c518716c03c29f89bcc
Bela Jash <abelxotessema@gmail.com>
2024-07-23 00:03:29 +0300

    Style email templates.

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Html, Body, Container, Text, Link, Preview} from '@react-email/components'
+import React, {CSSProperties} from 'react';
+import {Html, Body, Container, Text, Link, Preview, Tailwind} from '@react-email/components'
 
 interface Props {
   name: string
@@ -9,14 +9,26 @@ function WelcomeTemplate({name}: Props) {
   return (
     <Html>
       <Preview>Welcome aboard!</Preview>
-      <Body>
-        <Container>
-          <Text>Yahallo, {name}!</Text>
-          <Link href='https://game-hub-jet-eight.vercel.app/'>Game Hub</Link>
-        </Container>
-      </Body>
+      <Tailwind>
+        {/*<Body style={body}>*/}
+        <Body className='bg-gray-100'>
+          <Container>
+            {/*<Text style={heading}>Yahallo, {name}!</Text>*/}
+            <Text className='font-bold text-3xl'>Yahallo, {name}!</Text>
+            <Link href='https://game-hub-jet-eight.vercel.app/'>Game Hub</Link>
+          </Container>
+        </Body>
+      </Tailwind>
     </Html>
   );
+}
+
+const body: CSSProperties = {
+  background: '#ddd'
+}
+
+const heading: CSSProperties = {
+  fontSize: 'xx-large'
 }
 
 export default WelcomeTemplate;
