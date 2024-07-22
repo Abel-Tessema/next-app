5bd587b520e978bc0704f4be6a6a71381d16d116
Bela Jash <abelxotessema@gmail.com>
2024-07-23 01:46:38 +0300

    Use and optimize fonts.

--- app/page.tsx
+++ app/page.tsx
@@ -5,20 +5,22 @@ export default async function Home() {
   return (
     <main className='relative h-screen'>
       {/*<Image src={animeGirl} alt='Anime girl' />*/}
-      <Image
-        src='https://bit.ly/react-cover'
-        alt='Anime girl'
-        // width={300}
-        // height={170}
-        fill
-        // style={{objectFit: 'contain'}}
-        // style={{objectFit: 'cover'}}
-        className='object-cover'
-        // sizes='100vw'
-        sizes='(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw'
-        quality={100} // 1-100. Default is 75.
-        priority
-      />
+      {/*<h1 className='font-poppins'>Yahallo!</h1>*/}
+      <h1>Yahallo!</h1>
+      {/*<Image*/}
+      {/*  src='https://bit.ly/react-cover'*/}
+      {/*  alt='Anime girl'*/}
+      {/*  // width={300}*/}
+      {/*  // height={170}*/}
+      {/*  fill*/}
+      {/*  // style={{objectFit: 'contain'}}*/}
+      {/*  // style={{objectFit: 'cover'}}*/}
+      {/*  className='object-cover'*/}
+      {/*  // sizes='100vw'*/}
+      {/*  sizes='(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw'*/}
+      {/*  quality={100} // 1-100. Default is 75.*/}
+      {/*  priority*/}
+      {/*/>*/}
     </main>
   )
 }
